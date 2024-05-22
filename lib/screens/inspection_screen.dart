fa6352da4d3a2d8df47d921cbb81aa1a280d4306
dac0n <daniel.konstantinov.work@gmail.com>
2024-05-23 02:55:17 +0300

    removed prev / next btns overlapping animation when users tries to click too fast

--- lib/screens/inspection_screen.dart
+++ lib/screens/inspection_screen.dart
@@ -5,7 +5,7 @@ import 'package:my_golf_app/widgets/chart_container.dart';
 import 'package:my_golf_app/widgets/header.dart';
 import 'package:my_golf_app/widgets/navigation_button.dart';
 
-class InspectionScreen extends StatelessWidget {
+class InspectionScreen extends StatefulWidget {
   final Swing swing;
   final int index;
   final int totalSwings;
@@ -22,6 +22,47 @@ class InspectionScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _InspectionScreenState createState() => _InspectionScreenState();
+}
+
+class _InspectionScreenState extends State<InspectionScreen>
+    with SingleTickerProviderStateMixin {
+  bool _isAnimating = false;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300), // Animation duration
+    )..addStatusListener((status) {
+        if (status == AnimationStatus.completed ||
+            status == AnimationStatus.dismissed) {
+          setState(() {
+            _isAnimating = false;
+          });
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _navigate(int newIndex) {
+    if (!_isAnimating) {
+      setState(() {
+        _isAnimating = true;
+      });
+      widget.onNavigate(newIndex);
+      _animationController.forward(from: 0);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context),
@@ -33,26 +74,29 @@ class InspectionScreen extends StatelessWidget {
               CrossAxisAlignment.start, // Align all children to the start
           children: [
             HeaderWidget(
-              index: index,
-              totalSwings: totalSwings,
-              onDelete: onDelete,
-              onNavigate: onNavigate,
+              index: widget.index,
+              totalSwings: widget.totalSwings,
+              onDelete: widget.onDelete,
+              onNavigate: widget.onNavigate,
             ),
             const Text('Swing graph'),
-            ChartContainer(swing: swing),
+            ChartContainer(swing: widget.swing),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 NavigationButton(
-                  onPressed: index > 0 ? () => onNavigate(index - 1) : null,
+                  onPressed: widget.index > 0 && !_isAnimating
+                      ? () => _navigate(widget.index - 1)
+                      : null,
                   label: 'Previous',
                   icon: Icons.arrow_left,
                   isPrevious: true,
                 ),
                 NavigationButton(
-                  onPressed: index < totalSwings - 1
-                      ? () => onNavigate(index + 1)
-                      : null,
+                  onPressed:
+                      widget.index < widget.totalSwings - 1 && !_isAnimating
+                          ? () => _navigate(widget.index + 1)
+                          : null,
                   label: 'Next',
                   icon: Icons.arrow_right,
                   isPrevious: false,
